53b39d944b785f3b2707e6b204609a120576bd91
DESKTOP-3LG75LF\Quraishi <quraishiabdullah1994@gmail.com>
2020-05-10 09:48:51 +0600

    package-json-updated

--- test/index.js
+++ test/index.js
@@ -1,21 +1,22 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
+import app from '../app';
 
 const {expect} = chai;
 
 chai.use(chaiHttp);
 
-describe('Posts',() => {
-    describe('GET /posts',() => {
-        it('should return an array of all the posts', (done) => {
+describe('Movies',() => {
+    describe('GET /movies',() => {
+        it('should return an array of all the movies', (done) => {
             chai.request('https://jsonplaceholder.typicode.com/posts')
                 .get('/')
                 .end((err,res) => {
                     if(err) done(err);
                     expect(res).to.have.status(200);
-                    expect(res).to.be.an('object');
-                    //expect(res.body.status).to.deep.equals('success');
-                     //expect(res.body.posts).to.be.an('array');
+                    //expect(res).to.be.an('object');
+                    // expect(res.body.status).to.deep.equals('success');
+                    // expect(res.body.movies).to.be.an('array');
                      done();
                 })
         })
